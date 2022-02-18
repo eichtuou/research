4013ef4a0099fd21744c2b72dd35da27d230ceb2
eichtuou <sixfacedcube@gmail.com>
2022-02-18 15:06:36 -0500

    updated code to python3

--- fchk2xyz/fchk2xyz.py
+++ fchk2xyz/fchk2xyz.py
@@ -1,31 +1,29 @@
-'''
+"""
 Author: Jessica M. Gonzalez-Delgado
-	North Carolina State University
+        North Carolina State University
 
 This script translates an FCHK file to an XYZ file. Both filetypes
 contain molecular structure information. FCHK is specific from
 Gaussian09 while XYZ is widely used and can be read by various
-molecular modeling softwares.  
+molecular modeling softwares.
 
 Run as: python fchk2xyz.py file.fchk
-'''
+"""
 
 import sys
-import os
-from itertools import product
-from itertools import islice 
+from itertools import islice
 
 
 # check input file
-def check():
-    if len(sys.argv) == 0:
-        print "Error! No FCHK file specified."
-        print "Exiting now..."
+def check(argvs):
+    if len(argvs) == 0:
+        print("Error! No FCHK file specified.")
+        print("Exiting now...")
         sys.exit()
 
-    if sys.argv[1][-4:] != "FCHK": 
-        print "Error! File is not FCHK format."
-        print "Exiting now..."
+    if argvs[1][-4:] != "fchk":
+        print("Error! File is not FCHK format.")
+        print("Exiting now...")
         sys.exit()
 
     fchkfile = sys.argv[1]
@@ -35,86 +33,110 @@ def check():
 
 # get indeces of lines of interest on fchk file
 def get_index(ifile):
-    flags={}
+    flags = {}
     with open(ifile) as fo:
-        forceF=0
+        forceF = 0
         for num, line in enumerate(fo, 1):
             if 'Number of basis functions' in line:
-                flags.update({'nbasis':num})
+                flags.update({'nbasis': num})
+
             elif 'Nuclear charges' in line:
-                flags.update({'nuclCh':num})
+                flags.update({'nuclCh': num})
+
             elif 'Current cartesian coordinates' in line:
-                flags.update({'cartCord':num})
-            elif 'Force Field' in line: # for stop 
-                if forceF==0:
-                    forceF=num
-                    flags.update({'forceF':num})
+                flags.update({'cartCord': num})
+
+            elif 'Force Field' in line:  # for stop
+                if forceF == 0:
+                    forceF = num
+                    flags.update({'forceF': num})
     return flags
 
 
-# make xyz file from input fchk file 
+# make xyz file from input fchk file
 # user should expand dictionary at convenience
-def make_xyz(ifile,indx):
-    elem={'1':'H',\
-          '6':'C','7':'N','8':'O','9':'F',\
-          '15':'P','16':'S','17':'Cl',\
-          '22':'Ti','26':'Fe','27':'Co','28':'Ni','29':'Cu','30':'Zn','35':'Br',\
-          '44':'Ru','53':'I'}
-    angs=0.529177249
-    nucl=[]
-    clst=[]
+def make_xyz(ifile, indx):
+    elem = {'1': 'H',
+            '6': 'C', '7': 'N', '8': 'O', '9': 'F',
+            '15': 'P', '16': 'S', '17': 'Cl',
+            '22': 'Ti', '26': 'Fe', '27': 'Co', '28': 'Ni', '29': 'Cu',
+            '30': 'Zn', '35': 'Br', '44': 'Ru', '53': 'I'}
+    angs = 0.529177249
+    nucl = []
+    clst = []
 
     for key in indx:
         if key == 'nuclCh':
-            a=indx[key] 
+            a = indx[key]
         elif key == 'cartCord':
-            b=indx[key] 
+            b = indx[key]
         elif key == 'forceF':
-            c=indx[key] 
+            c = indx[key]
 
     with open(ifile) as lines:
-        for line in islice(lines,a,b-1):
-            buff=line.strip('\n')
-            buff=buff.split()
+        for line in islice(lines, a, b-1):
+            buff = line.strip('\n')
+            buff = buff.split()
             for i in range(len(buff)):
-                buff[i]=str(int(float(buff[i])))
+                buff[i] = str(int(float(buff[i])))
             for key in elem:
                 for i in range(len(buff)):
-                    buff[i] = buff[i].replace(key,elem[key])
+                    buff[i] = buff[i].replace(key, elem[key])
             for i in range(len(buff)):
                 nucl.append(buff[i])
 
     with open(ifile) as lines:
-        for line in islice(lines,b,c-1):
-            buff=line.strip('\n')
-            buff=buff.split()
+        for line in islice(lines, b, c-1):
+            buff = line.strip('\n')
+            buff = buff.split()
             for i in range(len(buff)):
                 clst.append(float(buff[i])*angs)
 
     if len(clst) != 3*len(nucl):
-        print 'Error! Number of coordinates doesn\'t match number of atoms.\nTerminating program.' 
+        print("Error! Number of coordinates doesn't match number of atoms"
+              "\nTerminating program.")
         sys.exit()
-    ofile=open(ifile[:-5]+'.xyz','w')
-    ofile.write(str(len(nucl))+'\n'+ifile+'\n')
-    i=0
 
-    for j in range(0,len(clst),3):
-        clst[j]="{0:.10f}".format(float(clst[j]))
-        clst[j+1]="{0:.10f}".format(float(clst[j+1]))
-        clst[j+2]="{0:.10f}".format(float(clst[j+2]))
-        buff=str(nucl[i])+'          '+str(clst[j])+'    '+str(clst[j+1])+'    '+str(clst[j+2])+'\n'
+    ofile = open(ifile[:-5]+'.xyz', 'w')
+    ofile.write(str(len(nucl))+'\n'+ifile+'\n')
+
+    i = 0
+    for j in range(0, len(clst), 3):
+        clst[j] = "{0:.10f}".format(float(clst[j]))
+        clst[j+1] = "{0:.10f}".format(float(clst[j+1]))
+        clst[j+2] = "{0:.10f}".format(float(clst[j+2]))
+
+        ws = '          '  # whitespace
+
+        if float(clst[j]) < 0:
+            clst[j] = ws[:-1]+clst[j]
+        else:
+            clst[j] = ws+clst[j]
+
+        if float(clst[j+1]) < 0:
+            clst[j+1] = ws[:-7]+clst[j+1]
+        else:
+            clst[j+1] = ws[:-6]+clst[j+1]
+
+        if float(clst[j+2]) < 0:
+            clst[j+2] = ws[:-7]+clst[j+2]
+        else:
+            clst[j+2] = ws[:-6]+clst[j+2]
+
+        buff = nucl[i] + clst[j] + clst[j+1] + clst[j+2] + '\n'
+
         ofile.write(buff)
-        i+=1
+        i += 1
+
     ofile.close()
 
 
 # MAIN PROGRAM
-def main():
-    fchk=check()
-    make_xyz(fchkf,get_index(fchkf))
-
+def main(argvs):
+    fchk = check(argvs)
+    make_xyz(fchk, get_index(fchk))
 
 
 # RUN PROGRAM
-main()
-
+argvs = sys.argv
+main(argvs)
