ffbce8823d70bcd8953a7c1b76c981262228e14a
eichtuou <sixfacedcube@gmail.com>
2023-01-21 18:42:20 -0500

    updated to python 3.11.1

--- xml2list/xml2list.py
+++ xml2list/xml2list.py
@@ -15,13 +15,13 @@ import sys
 # check input file
 def check():
     if len(sys.argv) == 0:
-        print "Error! No XML file specified."
-        print "Exiting now..."
+        print("Error! No XML file specified.")
+        print("Exiting now...")
         sys.exit()
-
+    
     if sys.argv[1][-3:] != "xml": 
-        print "Error! File is not XML format."
-        print "Exiting now..."
+        print("Error! File is not XML format.")
+        print("Exiting now...")
         sys.exit()
 
     xfile = sys.argv[1]
@@ -39,10 +39,9 @@ def getpeakinfo(xfile):
                 continue
             else:
                 line = line.split()
-                if line[0] == "<Peak2D": 
+                if line[0] == "<Peak2D":
                     peaks.append(line)
     return peaks
-
 
 # write LIST file
 def makelistfile(xfile,peaks):
@@ -51,7 +50,7 @@ def makelistfile(xfile,peaks):
     fo.write("Assignment    w1    w2    Data Height\n")
     for i in range(0,len(peaks)):
         buff = peaks[i][3].replace('"','')[11:]+"    "+peaks[i][1].replace('"','')[3:]+"    "+peaks[i][2].replace('"','')[3:]+"    "+peaks[i][4].replace('"','')[10:]+"\n"
-        fo.write(buff) 
+        fo.write(buff)
     fo.close()
 
 
@@ -63,4 +62,3 @@ def main():
 
 # RUN PROGRAM
 main()
-
